67ad0c032a9bbc813c48d298484bab8f14d6e2dc
shnam <shnamacedev@gmail.com>
2016-07-27 16:52:11 +0000

    first_commit

---
+++ shnam_app/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+import datetime
+from django.utils import timezone
+# Create your models here.
+
+class User(models.Model):
+	userIdx = models.AutoField(primary_key=True, db_index=True)
+	userId = models.CharField(unique=True, max_length=50)
+	email = models.CharField(max_length=50, blank=True, null=True)
+
+	device = models.CharField(max_length=50, blank=True, null=True)
+	loginToken = models.TextField(default="", blank=True)
+	createdTime = models.DateTimeField(auto_now_add=True)
+	disabled = models.BooleanField(default=False)
+
+
