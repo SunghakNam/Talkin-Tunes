4932704f4963ec47f72783874c68d90ba4b3d819
shnam <shnamacedev@gmail.com>
2016-07-28 07:04:25 +0000

    20160728_1

--- shnam/settings.py
+++ shnam/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'shnam_app/static/')
 STATIC_URL = '/static/'
--- shnam/urls.py
+++ shnam/urls.py
@@ -1,3 +1,4 @@
+
 """shnam URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'', include('shnam_app.urls')),
     url(r'^admin/', admin.site.urls),
+    
 ]
